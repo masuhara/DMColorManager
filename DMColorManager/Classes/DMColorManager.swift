3aa8ae99043d6aa52c89c1b4f943a1ef4e72a233
masuhara <info@masuhara.net>
2016-09-24 17:06:25 +0900

    Add Colors to lib

---
+++ DMColorManager/Classes/DMColorManager.swift
@@ -0,0 +1,91 @@
+//
+//  DMColorManager.swift
+//  TwitterManager-iOS
+//
+//  Created by Masuhara on 2016/09/24.
+//  Copyright © 2016年 net.maushara. All rights reserved.
+//
+
+import UIKit
+
+public class DMColorManager {
+    
+    // Twitter
+    public class func twitterBlue() -> UIColor {
+        return UIColor(red: 29.0 / 255.0, green: 202.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+    }
+    
+    class func twitterLogoBlue() -> UIColor {
+        return UIColor(red: 0.0 / 255.0, green: 172.0 / 255.0, blue: 237.0 / 255.0, alpha: 1.0)
+    }
+    
+    class func twitterDarkBlue() -> UIColor {
+        return UIColor(red: 0.0 / 255.0, green: 132.0 / 255.0, blue: 180.0 / 255.0, alpha: 1.0)
+    }
+    
+    class func twitterBackgroundBlue() -> UIColor {
+        return UIColor(red: 192.0 / 255.0, green: 222.0 / 255.0, blue: 237.0 / 255.0, alpha: 1.0)
+    }
+    
+    // Facebook
+    class func facebookBlue() -> UIColor {
+        return UIColor(red: 59.0 / 255.0, green: 89.0 / 255.0, blue: 152.0 / 255.0, alpha: 1.0)
+    }
+    
+    class func facebookMediumBlue() -> UIColor {
+        return UIColor(red: 109.0 / 255.0, green: 32.0 / 255.0, blue: 180.0 / 255.0, alpha: 1.0)
+    }
+    
+    class func facebookLightBlue() -> UIColor {
+        return UIColor(red: 175.0 / 255.0, green: 189.0 / 255.0, blue: 212.0 / 255.0, alpha: 1.0)
+    }
+    
+    class func facebookMessengerBlue() -> UIColor {
+        return UIColor(red: 18.0 / 255.0, green: 104.0 / 255.0, blue: 249.0 / 255.0, alpha: 1.0)
+    }
+    
+    // YouTube
+    class func youtubeRed() -> UIColor {
+        return UIColor(red: 187.0 / 255.0, green: 0.0 / 255.0, blue: 0.0 / 255.0, alpha: 1.0)
+    }
+    
+    // Google+
+    class func googleplusRed() -> UIColor {
+        return UIColor(red: 221.0 / 255.0, green: 75.0 / 255.0, blue: 57.0 / 255.0, alpha: 1.0)
+    }
+    
+    // LinkedIn
+    class func linkedinBlue() -> UIColor {
+        return UIColor(red: 0.0 / 255.0, green: 136.0 / 255.0, blue: 181.0 / 255.0, alpha: 1.0)
+    }
+    
+    // Instagram
+    class func instagramBlue() -> UIColor {
+        return UIColor(red: 18.0 / 255.0, green: 86.0 / 255.0, blue: 136.0 / 255.0, alpha: 1.0)
+    }
+    
+    // Pinterest
+    class func pinterestRed() -> UIColor {
+        return UIColor(red: 203.0 / 255.0, green: 32.0 / 255.0, blue: 39.0 / 255.0, alpha: 1.0)
+    }
+    
+    // Snapchat
+    class func snapchatYellow() -> UIColor {
+        return UIColor(red: 255.0 / 255.0, green: 252.0 / 255.0, blue: 180.0 / 255.0, alpha: 1.0)
+    }
+    
+    // DropBox
+    class func dropboxBlue() -> UIColor {
+        return UIColor(red: 0.0 / 255.0, green: 126.0 / 255.0, blue: 229.0 / 255.0, alpha: 1.0)
+    }
+    
+    // Flickr
+    class func flickrPink() -> UIColor {
+        return UIColor(red: 255.0 / 255.0, green: 0.0 / 255.0, blue: 132.0 / 255.0, alpha: 1.0)
+    }
+    
+    // Vine
+    class func vineGreen() -> UIColor {
+        return UIColor(red: 0.0 / 255.0, green: 191.0 / 255.0, blue: 143.0 / 255.0, alpha: 1.0)
+    }
+}
--- Example/DMColorManager/ViewController.swift
+++ Example/DMColorManager/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import DMColorManager
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = DMColorManager.twitterBlue()
     }
 
     override func didReceiveMemoryWarning() {
